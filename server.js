fce8779fd5c3bbbf738374587910a4fd003c9a58
MunizSalinasVictor <a.23308051280659@cbtis128.edu.mx>
2025-05-19 22:49:42 -0600

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,6 @@ app.use(helmet());
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-const express = require('express');
 const app = express();
 const path = require('path');
 
